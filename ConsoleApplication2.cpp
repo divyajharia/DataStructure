25f4289fa04355067ca115f2433df0805340a33e
divya jharia <divyajhariadj@gmail.com>
2021-05-30 16:50:40 +0530

    Add project files.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	//// your code goes here
+//	//unsigned short int n{ 0 };
+//	//cin >> n;
+//	////int count{ 1 };
+//	//for (int i = n; i > 0; --i)
+//	//{
+//	//	for (int j = i; j <= n; j++)
+//	//	{
+//	//		cout << "* ";
+//	//		//count++;
+//	//	}
+//	//	
+//	//	cout << endl;
+//	//}
+//	int n{ 0 };
+//	cin >> n;
+//	for (int i = 0; i < n ; i++)
+//	{
+//		for (int j = 0; j < n*2; j++)
+//		{
+//			if (j <=i || j >=(n*2)-2*i)
+//				cout << "* ";
+//			else
+//				cout << " ";
+//		}
+//		cout << endl;
+//	}
+//	return 0;
+//}
